20356bc27c1ff0ffad2b4f1389c2591afa35672b
Ty <ruu16001@byui.edu>
2018-02-15 16:01:25 -0700

    2/15/18 Work on SellLand Method

--- TheCityOfAaron/src/thecityofaaron/control/CropControl.java
+++ TheCityOfAaron/src/thecityofaaron/control/CropControl.java
@@ -5,10 +5,35 @@
  */
 package thecityofaaron.control;
 
+import thecityofaaron.model.CropData;
+
 /**
  *
  * @author Tyden R
  */
 public class CropControl {
     
-}
+    //The sellLand method
+    //Purpose: To sell land
+    //Parameters: the price of land, the number of acres to sell
+    // and a reference to a CropData object
+    //Returns: the acres to sell must be positive
+    //and <= acresOwned
+    
+    public static int sellLand(int landPrice, int acresToSell, CropData cropData) {
+    
+        //if acresToSell < 0, return -1
+        //if acresToSell > acresOwned, return -1
+        //acresOwned = acresOwned - acresToSell
+        //wheatInStore = wheatInStore + acresToSell * landPrice
+        //return acresOwned
+        
+        //if acresToSell > acresOwned, return -1
+        int owned = cropData.getAcresOwned();
+        if(acresToSell > owned) {
+            return -1;
+        }
+    }
+    
+}    
+
